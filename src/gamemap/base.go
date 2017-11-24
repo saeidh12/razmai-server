f9b7149c286ce4a851533f6e38f478e39ef4482e
saeidh12 <saeidhejazi12@gmail.com>
2017-11-25 01:37:45 +0330

    completed gamemap package

--- src/gamemap/base.go
+++ src/gamemap/base.go
@@ -1,18 +1,13 @@
 package gamemap
 
 type Base struct {
-	occupying_player int
-	troop_count      int
-	attack_bonus     float64
-	defense_bonus    float64
+	Occupying_player int
+	Troop_count      int
+	Attack_bonus     float64
+	Defense_bonus    float64
+	Troop_bonus      int
+	Connections      []int
 }
 
-func (b Base) get_occupying_player() int  { return b.occupying_player }
-func (b Base) get_troop_count() int       { return b.troop_count }
-func (b Base) get_attack_bonus() float64  { return b.attack_bonus }
-func (b Base) get_defense_bonus() float64 { return b.defense_bonus }
-
-func (b *Base) set_occupying_player(occupying_player int) { b.occupying_player = occupying_player }
-func (b *Base) set_troop_count(troop_count int)           { b.troop_count = troop_count }
-func (b *Base) set_attack_bonus(attack_bonus float64)     { b.attack_bonus = attack_bonus }
-func (b *Base) set_defense_bonus(defense_bonus float64)   { b.defense_bonus = defense_bonus }
+func (b Base) get_attack_strength(troops int) int { return int(b.Attack_bonus * float64(troops)) }
+func (b Base) get_defense_strength() int          { return int(b.Defense_bonus * float64(b.Troop_count)) }
--- src/gamemap/mapgraph.go
+++ src/gamemap/mapgraph.go
@@ -1,1 +1,84 @@
 package gamemap
+
+import "encoding/json"
+
+type MapGraph struct {
+	Number_of_players int
+	Weak_delimiter    int
+	Medium_delimiter  int
+	Conquer_bonus     int
+	Bases             []Base
+}
+
+func (mg MapGraph) toJSON() string {
+	map_json, _ := json.Marshal(mg)
+	return string(map_json)
+}
+
+func (mg *MapGraph) fromJSON(map_json string) {
+	bytes := []byte(map_json)
+	json.Unmarshal(bytes, mg)
+}
+
+func (mg *MapGraph) attack(from int, to int, troops int) string {
+	if mg.Bases[from].Troop_count < 2 || !inRange(from, -1, len(mg.Bases)) || !inRange(to, -1, len(mg.Bases)) || troops < 0 || from == to {
+		return "Invalid move!"
+	}
+	if inSlice(to, mg.Bases[from].Connections) < 0 {
+		return "No connection!"
+	}
+
+	if troops >= mg.Bases[from].Troop_count {
+		troops = mg.Bases[from].Troop_count - 1
+	}
+	mg.Bases[from].Troop_count -= troops
+
+	attack_force := mg.Bases[from].get_attack_strength(troops)
+	defense_force := mg.Bases[to].get_defense_strength()
+
+	battle_result := attack_force - defense_force
+
+	if battle_result > 0 {
+		mg.Bases[to].Troop_count = battle_result + mg.Conquer_bonus
+		mg.Bases[to].Occupying_player = mg.Bases[from].Occupying_player
+	} else {
+		battle_result *= -1
+		if battle_result < mg.Bases[to].Troop_count {
+			mg.Bases[to].Troop_count = battle_result
+			if mg.Bases[to].Troop_count == 0 {
+				mg.Bases[to].Occupying_player = -1
+			}
+		}
+	}
+
+	return ""
+}
+
+func (mg MapGraph) support(from int, to int, troops int) string {
+	if mg.Bases[from].Troop_count < 2 || !inRange(from, -1, len(mg.Bases)) || !inRange(to, -1, len(mg.Bases)) || troops < 0 {
+		return "Incorrect input"
+	}
+	if inSlice(to, mg.Bases[from].Connections) < 0 {
+		return "No connection"
+	}
+
+	if troops >= mg.Bases[from].Troop_count {
+		troops = mg.Bases[from].Troop_count - 1
+	}
+	mg.Bases[from].Troop_count -= troops
+	mg.Bases[to].Troop_count += troops
+
+	return ""
+}
+
+func inSlice(value int, list []int) int {
+	for i, v := range list {
+		if v == value {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func inRange(value, a, b int) bool { return value > a && value < b }
